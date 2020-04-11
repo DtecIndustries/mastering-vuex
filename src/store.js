7f48748426f5acd183604548084fa7bf16e67583
Joachim Defour <joachim.defour@icloud.com>
2020-04-11 22:41:52 +0200

    pagination

--- src/store.js
+++ src/store.js
@@ -8,17 +8,35 @@ export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education' , 'food', 'community' ],
-    events: []
+    events: [],
+    totalEvents: 0
   },
   mutations: {
     ADD_EVENT(state, event){
       state.events.push(event)
+    },
+    SET_EVENTS(state, events){
+      state.events = events
+    },
+    SET_TOTAL_EVENTS(state, totalEvents){
+      state.totalEvents = totalEvents
     }
   },
   actions: {
     createEvent({commit}, event){
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({commit}, { perPage, page }){
+      EventService.getEvents(perPage, page)
+      .then(response => {
+        console.log("total events are " + response.headers['x-total-count'])
+        commit('SET_TOTAL_EVENTS', response.headers['x-total-count'])
+        commit('SET_EVENTS', response.data)
+      })
+      .catch(error => {
+        console.log("there was an error:", error.response)
       })
     }
   },
