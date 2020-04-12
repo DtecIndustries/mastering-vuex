fa8ba154c6445a088cf0410676f2a4df276204bd
Joachim Defour <joachim.defour@icloud.com>
2020-04-12 22:49:36 +0200

    api network error redirect

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
 import NotFound from './views/NotFound.vue'
+import NetworkIssue from './views/NetworkIssue.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
 
@@ -27,7 +28,13 @@ const router = new Router({
         store.dispatch('event/fetchEvent', routeTo.params.id).then((event) => {
           routeTo.params.event = event
           next()
-        }).catch(() => next({name: '404', params: { resource: 'event' }}))
+        }).catch((error) => {
+          if(error.response && error.response.status == 404){
+            next({name: '404', params: { resource: 'event' }})
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
       }
     },
     {
@@ -40,6 +47,11 @@ const router = new Router({
       name: '404',
       component: NotFound,
       props: true
+    },
+    {
+      path: 'network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
     },
     {
       //catch all route
